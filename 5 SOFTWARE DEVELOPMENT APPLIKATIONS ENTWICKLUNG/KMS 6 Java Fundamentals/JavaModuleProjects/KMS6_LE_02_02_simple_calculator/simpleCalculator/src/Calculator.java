7af09f80f79f7d0d761136fe604596b2feb45328
Marius-Vladimir Matei <052583@edu.szf.at>
2025-03-13 12:36:57 +0100

    base area and volume calculator

--- 5 SOFTWARE DEVELOPMENT APPLIKATIONS ENTWICKLUNG/KMS 6 Java Fundamentals/JavaModuleProjects/KMS6_LE_02_02_simple_calculator/simpleCalculator/src/Calculator.java
+++ 5 SOFTWARE DEVELOPMENT APPLIKATIONS ENTWICKLUNG/KMS 6 Java Fundamentals/JavaModuleProjects/KMS6_LE_02_02_simple_calculator/simpleCalculator/src/Calculator.java
@@ -1,9 +1,9 @@
-public class MathOperations {
+public class Calculator {
 
     private double number1;
     private double number2;
 
-    public MathOperations(double number1, double number2) {
+    public Calculator(double number1, double number2) {
         this.number1 = number1;
         this.number2 = number2;
     }
